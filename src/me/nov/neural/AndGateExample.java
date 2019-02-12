4f0b22999e37d0653ff3d6b98ebe48cb47b4b43e
GraxCode <grax@graxco.de>
2019-02-12 12:47:25 +0100

    allow different neuron size in hidden layers

--- src/me/nov/neural/AndGateExample.java
+++ src/me/nov/neural/AndGateExample.java
@@ -1,5 +1,7 @@
 package me.nov.neural;
 
+import java.util.Arrays;
+
 public class AndGateExample {
 	public static void main(String[] args) {
 		NeuralNetwork nn = new NeuralNetwork();
@@ -8,13 +10,13 @@ public class AndGateExample {
 		System.out.println("0 0 -> " + nn.compute(new double[] { 0, 0 }));
 		System.out.println("0 1 -> " + nn.compute(new double[] { 0, 1 }));
 		System.out.println("1 1 -> " + nn.compute(new double[] { 1, 1 }));
-		for (int i = 0; i < 50000; i++) {
+		for (int i = 0; i < 5000; i++) {
 			double error = 0;
 			error += nn.train(new double[] { 1, 0 }, 0);
-			error += nn.train(new double[] { 0, 0 }, 1);
-			error += nn.train(new double[] { 0, 1 }, 1);
+			error += nn.train(new double[] { 0, 0 }, 0);
+			error += nn.train(new double[] { 0, 1 }, 0);
 			error += nn.train(new double[] { 1, 1 }, 1);
-			if(i % 100 == 0) {
+			if(i % 1000 == 0) {
 				System.out.println("--------");
 				System.out.println("error: " + error);
 				System.out.println("1 0 -> " + nn.compute(new double[] { 1, 0 }));
@@ -28,5 +30,6 @@ public class AndGateExample {
 		System.out.println("0 0 -> " + nn.compute(new double[] { 0, 0 }));
 		System.out.println("0 1 -> " + nn.compute(new double[] { 0, 1 }));
 		System.out.println("1 1 -> " + nn.compute(new double[] { 1, 1 }));
+		System.out.println(Arrays.toString(nn.weightsO) + " " + Arrays.deepToString(nn.weightsHL1) + " " + Arrays.deepToString(nn.weightsHL2));
 	}
 }
--- src/me/nov/neural/NeuralNetwork.java
+++ src/me/nov/neural/NeuralNetwork.java
@@ -3,46 +3,47 @@ package me.nov.neural;
 import java.util.Random;
 
 /**
- * This program is free software: 
- * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, 
- * either version 3 of the License, or (at your option) any later version. 
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
  * 
  * @author GraxCode
  */
 public class NeuralNetwork {
 	private static final int inputSize = 2;
 
-	private static final int hiddenLayerSize = 16;
-	private static final double learningRate = 0.05;
+	private static final int hiddenLayer1Size = 2;
+	private static final int hiddenLayer2Size = 1;
+	private static final double learningRate = 0.15;
 
 	private static final Random r = new Random();
-	private double[][] weightsHL1;
-	private double[][] weightsHL2;
-	private double[] weightsO;
+	public double[][] weightsHL1;
+	public double[][] weightsHL2;
+	public double[] weightsO;
 
 	public NeuralNetwork() {
-		weightsHL1 = new double[hiddenLayerSize][inputSize + 1]; // and bias
-		weightsHL2 = new double[hiddenLayerSize + 1][hiddenLayerSize + 1]; // and bias
-		weightsO = new double[hiddenLayerSize + 1]; // and bias
+		weightsHL1 = new double[hiddenLayer1Size][inputSize + 1]; // and bias
+		weightsHL2 = new double[hiddenLayer2Size + 1][hiddenLayer1Size + 1]; // and bias
+		weightsO = new double[hiddenLayer2Size + 1]; // and bias
 
-		for (int i = 0; i < hiddenLayerSize; i++) {
+		for (int i = 0; i < hiddenLayer1Size; i++) {
 			for (int j = 0; j < inputSize + 1; j++) {
 				weightsHL1[i][j] = r.nextDouble() * 2d - 1;
 			}
-			for (int j = 0; j < hiddenLayerSize + 1; j++) {
+		}
+		for (int i = 0; i < hiddenLayer2Size; i++) {
+			weightsO[i] = r.nextDouble() * 2d - 1;
+			for (int j = 0; j < hiddenLayer1Size + 1; j++) {
 				weightsHL2[i][j] = r.nextDouble() * 2d - 1;
-				weightsO[i] = r.nextDouble() * 2d - 1;
 			}
 		}
 	}
 
 	public double compute(double[] input) {
-		double[] hl1 = new double[hiddenLayerSize];
+		if (input.length != inputSize) {
+			throw new IllegalArgumentException();
+		}
+		double[] hl1 = new double[hiddenLayer1Size];
 
-		for (int i = 0; i < hiddenLayerSize; i++) {
+		for (int i = 0; i < hiddenLayer1Size; i++) {
 			double weightedValue = 0;
 			for (int j = 0; j < inputSize; j++) {
 				weightedValue += weightsHL1[i][j] * input[j];
@@ -51,84 +52,86 @@ public class NeuralNetwork {
 
 			hl1[i] = sigmoid(weightedValue);
 		}
-		double[] hl2 = new double[hiddenLayerSize];
-		for (int i = 0; i < hiddenLayerSize; i++) {
+		double[] hl2 = new double[hiddenLayer2Size];
+		for (int i = 0; i < hiddenLayer2Size; i++) {
 			double weightedValue = 0;
-			for (int j = 0; j < hiddenLayerSize; j++) {
+			for (int j = 0; j < hiddenLayer1Size; j++) {
 				weightedValue += weightsHL2[i][j] * hl1[j];
 			}
-			weightedValue += weightsHL2[i][hiddenLayerSize]; // bias
+			weightedValue += weightsHL2[i][hiddenLayer1Size]; // bias
 			hl2[i] = sigmoid(weightedValue);
 		}
 		double weightedValue = 0;
-		for (int j = 0; j < hiddenLayerSize; j++) {
+		for (int j = 0; j < hiddenLayer2Size; j++) {
 			weightedValue += weightsO[j] * hl2[j];
 		}
-		weightedValue += weightsO[hiddenLayerSize]; // bias
+		weightedValue += weightsO[hiddenLayer2Size]; // bias
 		return sigmoid(weightedValue);
 	}
 
 	public double train(double[] input, double expected) {
-		double[][] newWeightsHL1 = new double[hiddenLayerSize][inputSize + 1];
-		double[][] newWeightsHL2 = new double[hiddenLayerSize][hiddenLayerSize + 1];
-		double[] newWeightsO = new double[hiddenLayerSize + 1];
+		if (input.length != inputSize) {
+			throw new IllegalArgumentException(String.valueOf(input.length));
+		}
+		double[][] newWeightsHL1 = new double[hiddenLayer1Size][inputSize + 1];
+		double[][] newWeightsHL2 = new double[hiddenLayer2Size][hiddenLayer1Size + 1];
+		double[] newWeightsO = new double[hiddenLayer2Size + 1];
 
 		// forwardpropagation
-		double[] hl1 = new double[hiddenLayerSize];
-		for (int i = 0; i < hiddenLayerSize; i++) {
+		double[] hl1 = new double[hiddenLayer1Size];
+
+		for (int i = 0; i < hiddenLayer1Size; i++) {
 			double weightedValue = 0;
 			for (int j = 0; j < inputSize; j++) {
 				weightedValue += weightsHL1[i][j] * input[j];
 			}
 			weightedValue += weightsHL1[i][inputSize]; // bias
+
 			hl1[i] = sigmoid(weightedValue);
 		}
-		double[] hl2 = new double[hiddenLayerSize];
-		for (int i = 0; i < hiddenLayerSize; i++) {
+		double[] hl2 = new double[hiddenLayer2Size];
+		for (int i = 0; i < hiddenLayer2Size; i++) {
 			double weightedValue = 0;
-			for (int j = 0; j < hiddenLayerSize; j++) {
+			for (int j = 0; j < hiddenLayer1Size; j++) {
 				weightedValue += weightsHL2[i][j] * hl1[j];
 			}
-			weightedValue += weightsHL2[i][hiddenLayerSize]; // bias
+			weightedValue += weightsHL2[i][hiddenLayer1Size]; // bias
 			hl2[i] = sigmoid(weightedValue);
 		}
 		double weightedValue = 0;
-		for (int j = 0; j < hiddenLayerSize; j++) {
+		for (int j = 0; j < hiddenLayer2Size; j++) {
 			weightedValue += weightsO[j] * hl2[j];
 		}
-		weightedValue += weightsO[hiddenLayerSize];// bias
+		weightedValue += weightsO[hiddenLayer2Size]; // bias
 
 		// backpropagation
 		double output = sigmoid(weightedValue);
 		double deltaOutput = output * (1d - output) * (expected - output);
-		for (int i = 0; i < hiddenLayerSize; i++) {
+		for (int i = 0; i < hiddenLayer2Size; i++) {
 			newWeightsO[i] = weightsO[i] + deltaOutput * hl2[i] * learningRate;
 		}
-		newWeightsO[hiddenLayerSize] = weightsO[hiddenLayerSize] + deltaOutput * learningRate; // bias
+		newWeightsO[hiddenLayer2Size] = weightsO[hiddenLayer2Size] + deltaOutput * learningRate; // bias
 
 		// start with hl2
-		double[] deltasHl2 = new double[hiddenLayerSize + 1];
-		for (int i = 0; i < hiddenLayerSize; i++) {
-			double deltaHl2 = hl2[i] * (1 - hl2[i]) * (deltaOutput * weightsO[i]);
-			deltasHl2[i] = deltaHl2;
+		double[] deltasHl2 = new double[hiddenLayer2Size + 1];
+		for (int i = 0; i < hiddenLayer2Size; i++) {
+			deltasHl2[i] = hl2[i] * (1 - hl2[i]) * (deltaOutput * weightsO[i]);
 		}
-		for (int i = 0; i < hiddenLayerSize; i++) {
-			for (int j = 0; j < hiddenLayerSize; j++) {
+		for (int i = 0; i < hiddenLayer2Size; i++) {
+			for (int j = 0; j < hiddenLayer1Size; j++) {
 				newWeightsHL2[i][j] = weightsHL2[i][j] + deltasHl2[i] * hl1[j] * learningRate;
 			}
-			newWeightsHL2[i][hiddenLayerSize] = weightsHL2[i][hiddenLayerSize] + deltasHl2[i] * learningRate; // bias
+			newWeightsHL2[i][hiddenLayer1Size] = weightsHL2[i][hiddenLayer1Size] + deltasHl2[i] * learningRate; // bias
 		}
-		double[] deltasHl1 = new double[hiddenLayerSize];
-		for (int i = 0; i < hiddenLayerSize; i++) {
-
+		double[] deltasHl1 = new double[hiddenLayer1Size];
+		for (int i = 0; i < hiddenLayer1Size; i++) {
 			double sum = 0;
-			for (int k = 0; k < hiddenLayerSize; k++) {
+			for (int k = 0; k < hiddenLayer2Size; k++) {
 				sum += deltasHl2[k] * weightsHL2[k][i];
 			}
-			double deltaHl1 = hl1[i] * (1 - hl1[i]) * (sum);
-			deltasHl1[i] = deltaHl1;
+			deltasHl1[i] = hl1[i] * (1 - hl1[i]) * (sum);
 		}
-		for (int i = 0; i < hiddenLayerSize; i++) {
+		for (int i = 0; i < hiddenLayer1Size; i++) {
 			for (int j = 0; j < inputSize; j++) {
 				newWeightsHL1[i][j] = weightsHL1[i][j] + deltasHl1[i] * input[j] * learningRate;
 			}
@@ -138,7 +141,7 @@ public class NeuralNetwork {
 		weightsO = newWeightsO;
 		weightsHL1 = newWeightsHL1;
 		weightsHL2 = newWeightsHL2;
-		return Math.abs(expected - output);
+		return Math.pow((expected - output), 2) / 2d;
 	}
 
 	private double sigmoid(double value) {
